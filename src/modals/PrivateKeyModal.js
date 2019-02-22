b15db1a5d34c027d59a1055d53aa9ea4bc41717b
averytt <averywhere18@gmail.com>
2019-02-22 15:54:12 -0800

    Add missing files (PrivateKeyModal)

---
+++ src/modals/PrivateKeyModal.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './PrivateKeyModal.css';
+import Modal from '../components/Modal'
+import GradientButton from '../components/buttons/GradientButton'
+import {store} from "../state";
+import {hideModal} from "../state/actions/Modals";
+
+export default class PrivateKeyModal extends React.Component {
+    constructor(){
+        super();
+
+        this.close = this.close.bind(this);
+    }
+
+    close(){
+        store.dispatch(hideModal());
+    }
+
+    render() {
+        return (
+            <Modal>
+                <div className="PrivateKeyModal">
+                    <div className="PrivateKeyModal__title">
+                        Your Private Key
+                    </div>
+                    <div className="PrivateKeyModal__instructions">
+                        Backup the text below on paper or digitally and keep it somewhere safe and secure.
+                    </div>
+                    <textarea className="PrivateKeyModal__private-key"
+                              value={this.props.privateKey}
+                              readOnly={true}
+                    />
+                    <GradientButton title="Close"
+                                    onClick={this.close}
+                    />
+                </div>
+            </Modal>
+        )
+    }
+}
