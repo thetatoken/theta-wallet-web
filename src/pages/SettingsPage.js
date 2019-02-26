c11ead1b91aac8bf359dbcf393fb1717418f9116
averytt <averywhere18@gmail.com>
2019-02-26 10:50:48 -0800

    Added Settings page

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './SettingsPage.css';
+import PageHeader from "../components/PageHeader";
 
 class SettingsPage extends React.Component {
     render() {
-
         return (
             <div className="SettingsPage">
-                I am settings
+                <div className="SettingsPage__detail-view">
+                    <PageHeader title="Settings"
+                                sticky={true}
+                    />
+                </div>
             </div>
         );
     }
