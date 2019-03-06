f050132e768fe27744785a8265ad1f5e1d0e7df6
averytt <averywhere18@gmail.com>
2019-03-05 18:09:17 -0800

    Move token icons to prevent them flowing randomly

--- src/components/ThetaTransactionListItem.js
+++ src/components/ThetaTransactionListItem.js
@@ -45,16 +45,16 @@ class ThetaTransactionListItem extends React.Component {
 
                 <div className="ThetaTransactionListItem__right-container">
                     <div className="ThetaTransactionListItem__amount-container">
+                        <div className="ThetaTransactionListItem__amount">{thetaAmount}</div>
                         <img className="ThetaTransactionListItem__amount-icon"
                              src="/img/tokens/theta_large@2x.png"
                         />
-                        <div className="ThetaTransactionListItem__amount">{thetaAmount}</div>
                     </div>
                     <div className="ThetaTransactionListItem__amount-container">
+                        <div className="ThetaTransactionListItem__amount">{tfuelAmount}</div>
                         <img className="ThetaTransactionListItem__amount-icon"
                              src="/img/tokens/tfuel_large@2x.png"
                         />
-                        <div className="ThetaTransactionListItem__amount">{tfuelAmount}</div>
                     </div>
                 </div>
             </a>
