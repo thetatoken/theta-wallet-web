c1267c04a567e2e50040f7478bc632bad8d0d0de
Qinwei Gong <qinwei@sliver.tv>
2019-05-06 23:00:59 -0700

    more refactoring

--- src/services/Trezor.js
+++ src/services/Trezor.js
@@ -1,8 +1,4 @@
-import {BigNumber} from 'bignumber.js';
-import Ethereum from "./Ethereum";
 import ThetaJS from '../libs/thetajs.esm';
-import TokenTypes from "../constants/TokenTypes";
-import Config from '../Config';
 import TrezorConnect from 'trezor-connect';
 import Web3 from 'web3';
 import RLP from 'eth-lib/lib/rlp';
@@ -14,22 +10,8 @@ const rpcURL = "https://mainnet.infura.io/v3/40980e2189924c8abfc5f60dd2e5dc4b";
 const web3 = new Web3(rpcURL);
 
 export default class Trezor {
-
-    static getTransactionFee(){
-        //10^12 TFuelWei
-        return 0.000001;
-    }
-
-    static getChainID(){
-        return Config.thetaChainID;
-    }
-
-    static isAddress(address){
-        return Ethereum.isAddress(address);
-    }
-
     static async signTransaction(txData, sequence){
-        let chainID = Trezor.getChainID();
+        let chainID = Theta.getChainID();
         let unsignedTx = Theta.unsignedTransaction(txData, sequence);
 
         let encodedChainID = RLP.encode(Bytes.fromString(chainID));
