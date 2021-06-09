6bd71bad7fe1046557bd6da053d1502659ffbcf4
averytt <averywhere18@gmail.com>
2021-06-09 15:23:27 -0400

    Disable faucet

--- src/Config.js
+++ src/Config.js
@@ -3,7 +3,9 @@ import Networks from './constants/Networks'
 const config = {
     isThetaNetworkLive: true,
 
-    defaultThetaChainID: Networks.THETA_MAINNET
+    defaultThetaChainID: Networks.THETA_MAINNET,
+
+    faucetAvailable: false
 };
 
 export default config;
