51be3e13f8a6b9b7d567a6ffd134fd58231cafe3
jingxu85 <jing@sliver.tv>
2021-10-17 22:33:57 -0700

    use chainID 1 for ETH wrapper with Trezor wallet

--- src/services/Trezor.js
+++ src/services/Trezor.js
@@ -9,45 +9,13 @@ import Api from './Api'
 const rpcURL = "https://mainnet.infura.io/v3/40980e2189924c8abfc5f60dd2e5dc4b";
 const web3 = new Web3(rpcURL);
 
-
-async function localBoardcast(signedTxRaw) {
-    console.log("jlog0 local boardcasting", signedTxRaw)
-    var payload1 = {
-        "jsonrpc": "2.0", 
-        "method": "theta.BroadcastRawTransaction",
-        "params": [{"tx_bytes": signedTxRaw}], 
-        "id": 0
-    };
-    var payload2 = {
-        "jsonrpc": "2.0", 
-        "method": "theta.GetStatus",
-        "params": [{}], 
-        "id": 0
-    };
-    await fetch("http://localhost:16888/rpc", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload1),
-    }).then(response => response.json())
-    .then(data => console.log("jlog1", data));
-    await fetch("http://localhost:16888/rpc", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload2),
-    }).then(response => response.json())
-    .then(data => console.log("jlog2", data));
-
-}
-
 export default class Trezor {
     static async signTransaction(unsignedTx){
-        // let unsignedTx = Theta.unsignedSendTx(txData, sequence);
         let payload = Theta.prepareTxPayload(unsignedTx);
 
         const trezorSignParams = {
             path: Wallet.getWalletPath(),
             transaction: {
-                // chainId: chainIDStringToNumber(Theta.getChainID()),
                 chainId: 1,
                 nonce: web3.utils.toHex('0'),
                 gasPrice: web3.utils.toHex('0'),
@@ -61,11 +29,8 @@ export default class Trezor {
         if (signedTx.payload.error) {
             throw signedTx.payload.error;
         }
-
-        let signature = signedTx.payload.r + signedTx.payload.s.slice(2) + (parseInt(signedTx.payload.v, 16) - 27).toString().padStart(2, '0');
+        let signature = signedTx.payload.r + signedTx.payload.s.slice(2) + (parseInt(signedTx.payload.v, 16) - 37).toString().padStart(2, '0');
         unsignedTx.setSignature(signature);
-        console.log("jlog3 unsignedTX is ", unsignedTx);
-        console.log("jlog4 signedTx is ", signedTx);
         let signedRawTxBytes = ThetaJS.TxSigner.serializeTx(unsignedTx);
         let signedTxRaw = signedRawTxBytes.toString('hex');
 
@@ -73,7 +38,6 @@ export default class Trezor {
         // signedTxRaw = signedTxRaw.substring(2);
 
         if(signedTxRaw){
-            localBoardcast(signedTxRaw)
             return signedTxRaw;
         }
         else{
