6097816884b0c526aa628abd0f6a90f7a14a46aa
Adrien Vinches <ad.vinches@gmail.com>
2023-12-07 17:33:46 -0800

    transaction list

--- src/components/ThetaTransactionListItem.js
+++ src/components/ThetaTransactionListItem.js
@@ -5,21 +5,46 @@ import TransactionStatus from './TransactionStatus'
 import {formatNativeTokenAmountToLargestUnit, numberWithCommas, truncate} from "../utils/Utils";
 import _ from 'lodash';
 import Theta from '../services/Theta';
+import tns from "../libs/tns"
+import Wallet from "../services/Wallet";
 
 class ThetaTransactionListItem extends React.Component {
+    constructor(){
+        super();
+        this.state = { tnsName: false };
+    }
+
+    async componentDidMount() {
+        if(Object.keys(this.props.transaction).length !== 0) {
+            let {inputs, outputs} = this.props.transaction;
+            let address = Wallet.getWalletAddress();
+            let input = (inputs ? inputs[0] : null);
+            let output = (outputs ? outputs[0] : null);
+            let from = _.get(input, ['address']);
+            let to = _.get(output, ['address']);
+            const tnsName = await tns.getDomainName(
+                address.toLowerCase() === to.toLowerCase() ? from : to
+            );
+            this.setState({tnsName: tnsName});
+        }
+    }
+
     render() {
-        let { transaction } = this.props;
-        let {inputs, outputs, timestamp, bound, hash, is_local, chainId} = transaction;
+        let transaction = this.props.transaction;
+        let {inputs, outputs, timestamp, is_local} = transaction;
+        let address = Wallet.getWalletAddress();
+        
         let input = (inputs ? inputs[0] : null);
         let output = (outputs ? outputs[0] : null);
         let from = _.get(input, ['address']);
         let to = _.get(output, ['address']);
+        let bound = address.toLowerCase() === to.toLowerCase() ? "inbound" : "outbound";
         let isReceived = (bound === "inbound");
         let explorerUrl = Theta.getTransactionExplorerUrl(transaction);
 
-        //Truncate the addresses to help reduce the run ons
-        from = truncate(from);
-        to = truncate(to);
+        // //Truncate the addresses to help reduce the run ons
+        // from = truncate(from);
+        // to = truncate(to);
 
         let thetaAmount = _.get(output, ['coins', 'thetawei']);
         let tfuelAmount = _.get(output, ['coins', 'tfuelwei']);
@@ -36,7 +61,9 @@ class ThetaTransactionListItem extends React.Component {
                     <div className="ThetaTransactionListItem__middle-container">
                         <div className="ThetaTransactionListItem__address-container">
                             <div className="ThetaTransactionListItem__address-prefix" >{isReceived ? "FROM:" : "TO:"}</div>
-                            <div className="ThetaTransactionListItem__address">{isReceived ? from : to}</div>
+                            <div className="ThetaTransactionListItem__address">
+                                <TNS addr={isReceived ? from : to} tnsName={this.state.tnsName} />
+                            </div>
                         </div>
                     </div>
                     <div className="ThetaTransactionListItem__bottom-container">
@@ -69,5 +96,23 @@ class ThetaTransactionListItem extends React.Component {
         );
     }
 }
+
+const TNS = ({addr, tnsName}) => {
+    //Truncate the addresses to help reduce the run ons
+    let truncAddr = truncate(addr);
+
+    return (
+        <div className="value tooltip">
+            {tnsName &&
+            <div className="tooltip--text">
+                <p>
+                    {tnsName}<br/>
+                    ({addr})
+                </p>
+            </div>}
+            {tnsName ? tnsName : addr ? truncAddr : ''}
+        </div>
+    );
+};
 
 export default ThetaTransactionListItem;
