56424cba06918bc0c7fd0eb35311f6ef0b4a428b
averytt <averywhere18@gmail.com>
2024-01-13 21:43:53 -0800

    Quick get tnt20 tokens in tx list

--- src/components/ThetaTransactionListItem.js
+++ src/components/ThetaTransactionListItem.js
@@ -2,9 +2,15 @@ import React from "react";
 import './ThetaTransactionListItem.css';
 import moment from 'moment';
 import TransactionStatus from './TransactionStatus'
-import {formatNativeTokenAmountToLargestUnit, numberWithCommas, truncate} from "../utils/Utils";
+import {
+    formatNativeTokenAmountToLargestUnit,
+    formatTNT20TokenAmountToLargestUnit,
+    numberWithCommas,
+    truncate
+} from "../utils/Utils";
 import _ from 'lodash';
 import Theta from '../services/Theta';
+const {getKnownToken, getKnownTokens} = require('@thetalabs/wallet-metadata');
 
 class ThetaTransactionListItem extends React.Component {
     render() {
@@ -23,6 +29,11 @@ class ThetaTransactionListItem extends React.Component {
 
         let thetaAmount = _.get(output, ['coins', 'thetawei']);
         let tfuelAmount = _.get(output, ['coins', 'tfuelwei']);
+        let tokenName = _.get(output, ['coins', 'name']);
+        let tokenValue = _.get(output, ['coins', 'value']);
+        let tokenContractAddress = _.get(output, ['coins', 'contract_address']);
+        let tokenDecimals = _.get(output, ['coins', 'decimals']);
+        let knownToken = getKnownToken(chainId, tokenContractAddress || '');
 
         return (
             <a className="ThetaTransactionListItem"
@@ -44,27 +55,45 @@ class ThetaTransactionListItem extends React.Component {
                     </div>
                 </div>
 
-                <div className="ThetaTransactionListItem__right-container">
-                    {
-                        thetaAmount !== '0' &&
-                        <div className="ThetaTransactionListItem__amount-container">
-                            <div className="ThetaTransactionListItem__amount">{formatNativeTokenAmountToLargestUnit(thetaAmount)}</div>
-                            <img className="ThetaTransactionListItem__amount-icon"
-                                 src="/img/tokens/theta_large@2x.png"
-                            />
-                        </div>
-                    }
-                    {
-                        tfuelAmount !== '0' &&
-                        <div className="ThetaTransactionListItem__amount-container">
-                            <div className="ThetaTransactionListItem__amount">{formatNativeTokenAmountToLargestUnit(tfuelAmount)}</div>
-                            <img className="ThetaTransactionListItem__amount-icon"
-                                 src="/img/tokens/tfuel_large@2x.png"
-                            />
-                        </div>
-                    }
+                {
+                    tokenName &&
+                    <div className="ThetaTransactionListItem__right-container">
+                        {
+                            thetaAmount !== '0' &&
+                            <div className="ThetaTransactionListItem__amount-container">
+                                <div className="ThetaTransactionListItem__amount">{formatTNT20TokenAmountToLargestUnit(tokenValue, tokenDecimals)}</div>
+                                <img className="ThetaTransactionListItem__amount-icon"
+                                     src={knownToken.logoUrl}
+                                />
+                            </div>
+                        }
 
-                </div>
+                    </div>
+                }
+                {
+                    _.isNil(tokenName) &&
+                    <div className="ThetaTransactionListItem__right-container">
+                        {
+                            thetaAmount !== '0' &&
+                            <div className="ThetaTransactionListItem__amount-container">
+                                <div className="ThetaTransactionListItem__amount">{formatNativeTokenAmountToLargestUnit(thetaAmount)}</div>
+                                <img className="ThetaTransactionListItem__amount-icon"
+                                     src="/img/tokens/theta_large@2x.png"
+                                />
+                            </div>
+                        }
+                        {
+                            tfuelAmount !== '0' &&
+                            <div className="ThetaTransactionListItem__amount-container">
+                                <div className="ThetaTransactionListItem__amount">{formatNativeTokenAmountToLargestUnit(tfuelAmount)}</div>
+                                <img className="ThetaTransactionListItem__amount-icon"
+                                     src="/img/tokens/tfuel_large@2x.png"
+                                />
+                            </div>
+                        }
+
+                    </div>
+                }
             </a>
         );
     }
