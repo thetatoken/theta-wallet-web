9f2f70c26f1373ce4e122cd1d61394c5a162ded4
James <james@thetalabs.org>
2026-07-14 21:48:38 +0900

    Remove unnecessary vm polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -10,7 +10,6 @@ module.exports = function override(config) {
         https: require.resolve("https-browserify"),
         os: require.resolve("os-browserify"),
         url: require.resolve("url"),
-        vm: require.resolve("vm-browserify"),
         path: false
     });
     config.resolve.fallback = fallback;
